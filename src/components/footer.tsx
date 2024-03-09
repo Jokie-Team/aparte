5e1b21755d143ec1709d40afda794b1cf445e6ba
Maria João Mira Paulo <maria.joao@fidel.uk>
2024-03-09 18:57:53 +0000

    Add dropdown buttons

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,11 @@
-import { ArrowBackIcon } from "./icons/arrow-back";
+import DropdownButton from "./buttons/dropdown";
+
 export default function Footer() {
   return (
     <footer className="bg-accent-1 border-t border-accent-2">
       <div className="container mx-auto px-5">
         <div className="py-28 flex flex-col lg:flex-row items-center">
-          <p className="font-neue text-orange">This is a footer</p>
-          <ArrowBackIcon />
+          <DropdownButton />
         </div>
       </div>
     </footer>
