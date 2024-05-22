e2c34c8ff95f686e09521f16a12b4b14c7119b03
Francisca Paupério <franciscapauperio@gmail.com>
2024-05-22 09:21:13 +0100

    fix footer at the bottom

--- src/app/[lng]/(aparte)/contacts/page.tsx
+++ src/app/[lng]/(aparte)/contacts/page.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import { useTranslation } from '../../../i18n';
+import Button from '@/src/components/buttons/button';
+import DropdownButton from '@/src/components/buttons/dropdown';
+import ForwardButton from '@/src/components/buttons/forward';
+import IconButton from '@/src/components/buttons/icon';
+import TextButton from '@/src/components/buttons/text';
 
 const Contacts = async ({ params }: { params: { lng: string } }) => {
     const { t } = await useTranslation(params.lng, 'common')
@@ -7,7 +12,30 @@ const Contacts = async ({ params }: { params: { lng: string } }) => {
     return (
         <div>
             <h1>{t('contacts')}</h1>
-            {/* Page Content */}
+            <div className="container mx-auto px-5">
+                <div className="py-28 flex flex-col space-y-10">
+                    <div>
+                        <div>Dropdown Button</div>
+                        <DropdownButton />
+                    </div>
+                    <div>
+                        <div>Button</div>
+                        <Button text="teste" />
+                    </div>
+                    <div>
+                        <div>Forward Button</div>
+                        <ForwardButton text="label" />
+                    </div>
+                    <div>
+                        <div>Icon Button</div>
+                        <IconButton direction="left" />
+                    </div>
+                    <div>
+                        <div>Text Button</div>
+                        <TextButton text="Label" />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,36 +1,8 @@
-import DropdownButton from "./buttons/dropdown";
-import Button from "./buttons/button";
-import ForwardButton from "./buttons/forward";
-import IconButton from "./buttons/icon";
-import TextButton from "./buttons/text";
+import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className="bg-accent-1 border-t border-accent-2">
-      <div className="container mx-auto px-5">
-        <div className="py-28 flex flex-col space-y-10">
-          <div>
-            <div>Dropdown Button</div>
-            <DropdownButton />
-          </div>
-          <div>
-            <div>Button</div>
-            <Button text="teste" />
-          </div>
-          <div>
-            <div>Forward Button</div>
-            <ForwardButton text="label" />
-          </div>
-          <div>
-            <div>Icon Button</div>
-            <IconButton direction="left" />
-          </div>
-          <div>
-            <div>Text Button</div>
-            <TextButton text="Label" />
-          </div>
-        </div>
-      </div>
+    <footer className="bg-accent-1 border-t border-accent-2 text-center p-4 mt-auto">
     </footer>
   );
 }
